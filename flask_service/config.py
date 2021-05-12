90946b31a7ee461599c714572c4ae92e97ec1d9a
valeria_mozharova <joinmek@rambler.ru>
2021-05-12 22:59:04 +0300

    add loading model

--- flask_service/config.py
+++ flask_service/config.py
@@ -1,1 +1,3 @@
 DATA_PATH = 'data/heart.csv'
+MODEL_PATH = 'models/heart.pkl'
+MODEL_INFO_PATH = 'models/heart_info.json'
--- flask_service/train_model.py
+++ flask_service/train_model.py
@@ -1,19 +1,30 @@
+import json
 import pandas as pd
+import pickle as pkl
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import f1_score
 
-from config import DATA_PATH
+from config import DATA_PATH, MODEL_PATH, MODEL_INFO_PATH
 
-cls = None
-model_info = None
+
+model, model_info = None, None
 
 
 def get_model():
-    global cls, model_info
-    if cls is not None:
-        return cls, model_info
+    global model, model_info
+    if model is not None:
+        return model, model_info
+    with open(MODEL_PATH, mode='rb') as in_:
+        model = pkl.load(in_)
+    with open(MODEL_INFO_PATH, encoding='utf8') as in_:
+        model_info = json.load(in_)
+
+    return model, model_info
+
+
+def train_model():
     data_set = pd.read_csv(DATA_PATH)
     target_col = 'target'
     feature_cols = [col for col in data_set.columns if col != target_col]
@@ -21,10 +32,15 @@ def get_model():
                                                         stratify=data_set[target_col], test_size=0.2)
     cls = GradientBoostingClassifier()
     cls.fit(X_train, y_train)
+    with open(MODEL_PATH, mode='wb') as out:
+        pkl.dump(cls, out)
     prediction = cls.predict(X_test)
     score = float(f1_score(y_test, prediction))
     model_info = {'model_name': 'Gradient Boosting', 'f1-score': score, 'required_cols': feature_cols}
     print(score, flush=True)
+    with open(MODEL_INFO_PATH, encoding='utf8', mode='w') as out:
+        json.dump(model_info, out)
 
-    return cls, model_info
 
+if __name__ == '__main__':
+    train_model()
